f9abe32bdc179d7868b8dc28683db5c13f6cbe6a
李彬 <civet148@126.com>
2021-11-10 09:25:34 +0800

    rename variant of digitBytes to digestHash

--- api.go
+++ api.go
@@ -7,8 +7,8 @@ const (
 type Wallet interface {
 	Create() Wallet                                              //create a new wallet
 	Recover(strPhrase string) Wallet                             //recover wallet from phrase
-	SignHash(digitBytes []byte) (strSignature string, err error) //sign hash bytes (32bytes) and return a hex signature string
-	VerifyHash(digitBytes []byte, strSignature string) bool      //verify hash bytes (32bytes) signature from a hex encoded signature
+	SignHash(digestHash []byte) (strSignature string, err error) //sign hash bytes (32bytes) and return a hex signature string
+	VerifyHash(digestHash []byte, strSignature string) bool      //verify hash bytes (32bytes) signature from a hex encoded signature
 	SignText(text []byte) (strSignature string, err error)       //sign text bytes and return a hex signature string
 	VerifyText(text []byte, strSignature string) bool            //verify text bytes signature from a hex encoded signature
 	GetPrivateKey() string                                       //retrieve private key (hex string)
